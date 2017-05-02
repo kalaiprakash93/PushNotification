c4d2d38d1f3b2a414b5b17766754ebbee55955b6
subbuselfservit <saranga.subbulakshmi@selfservit.com>
2017-05-02 14:06:16 +0530

    Update mPushNotification.java

--- src/android/mPushNotification.java
+++ src/android/mPushNotification.java
@@ -8,50 +8,25 @@ import org.json.JSONException;
 import org.apache.cordova. * ;
 import org.json.JSONObject;
 
+import java.util.logging.Handler;
+
 public class mPushNotification extends CordovaPlugin {
-	private static CallbackContext tokenRefreshCallbackContext;
+	//private static CallbackContext tokenRefreshCallbackContext;
 
 	@Override
 	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		// FirebaseApp.initializeApp(cordova.getActivity());
 		if (action.equals("GetToken")) {
-			try {
-				String token = FirebaseInstanceId.getInstance().getToken();
-				callbackContext.success(token);
-			} catch (Exception e) {
-				callbackContext.error(e.getMessage());
-			}
-		}else if(action.equals("TokenRefresh")){
-			onTokenRefresh(callbackContext);
+			getToken(callbackContext);
 		}
 		return true;
 	}
-	private void onTokenRefresh(final CallbackContext callbackContext) {
-		mPushNotification.tokenRefreshCallbackContext = callbackContext;
-
-		cordova.getThreadPool().execute(new Runnable() {
-			public void run() {
-				try {
-					String currentToken = FirebaseInstanceId.getInstance().getToken();
-
-					if (currentToken != null) {
-						mPushNotification.sendToken(currentToken);
-					}
-				} catch (Exception e) {
-					callbackContext.error(e.getMessage());
-				}
-			}
-		});
-	}
-	public static void sendToken(String token) {
-		if (mPushNotification.tokenRefreshCallbackContext == null) {
-			return;
-		}
-		final CallbackContext callbackContext = mPushNotification.tokenRefreshCallbackContext;
-		if (callbackContext != null && token != null) {
-			PluginResult pluginresult = new PluginResult(PluginResult.Status.OK, token);
-			pluginresult.setKeepCallback(true);
-			callbackContext.sendPluginResult(pluginresult);
+	public static void getToken(CallbackContext callbackContext){
+		try {
+			String token = FirebaseInstanceId.getInstance().getToken();
+			callbackContext.success(token);
+		} catch (Exception e) {
+			callbackContext.error(e.getMessage());
 		}
 	}
 }
