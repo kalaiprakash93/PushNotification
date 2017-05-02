12035f62270e0d3644262624f5638b5f143ecda1
subbuselfservit <saranga.subbulakshmi@selfservit.com>
2017-05-02 11:48:20 +0530

    Update mPushNotification.java

--- src/android/mPushNotification.java
+++ src/android/mPushNotification.java
@@ -13,17 +13,12 @@ public class mPushNotification extends CordovaPlugin {
 	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext)throws JSONException {
 		FirebaseApp.initializeApp(cordova.getActivity());
 		if (action.equals("GetToken")) {
-			cordova.getThreadPool().execute(new Runnable() {
-			    public void run() {
 				try {
 				    String token = FirebaseInstanceId.getInstance().getToken();
 				    callbackContext.success(token);
 				} catch (Exception e) {
 				    callbackContext.error(e.getMessage());
 				}
-			    }
-			});
-			
 		}
 		return true;
 	}
